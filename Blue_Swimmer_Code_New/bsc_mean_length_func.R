a47e7a8a082637262955dd2ea3c4c161765bcfe7
lukelloydjones <luke.lloydjones@uqconnect.edu.au>
2015-01-19 20:34:51 +1000

    Updated BSC code to include new functions and be much more readable. Attempting to write nice new code that will be much less error prone and adheres to the Google coding standard

---
+++ Blue_Swimmer_Code_New/bsc_mean_length_func.R
@@ -0,0 +1,105 @@
+# Mean function for calcuating the mean length for the current month
+# ------------------------------------------------------------------
+
+
+MeanLength <- function(month, k0, theta.1, theta.2 , linf, mu.yr.1, mu.yr.2, 
+			  yrs.old, str.mnth) {
+	
+  # Computes the mean length of the distribution for the current month
+  # based on an integration over the seasonal curve from a von Bertal
+  # anffy growth model.
+  #
+  # Args:
+  #  month:    Current month to calulate mean at. Numbered from Jan=0
+  #  k0:       Mean  k paramter for VB model
+  #  theta.1:  Seasonality parameter 1
+  #  theta.2:  Seasonality parameter 1
+  #  linf:     Asymptotic length
+  #  mu.yr.1:  First month's average length yr 1
+  #  mu.yr.2:  First month's average length yr 2
+  #  yrs.old:  How old the individuals in this group are
+  #  str.mnth: The month that we start calculating means for 
+  #
+  # Returns:
+  #  Mean length for the month in the current iteration
+  
+  
+  # Set each of the months to be the middle of the month (1/24 to be used
+  # with the seasonal function.
+  
+  mm.val  <- (month %% 12) / 12 + 1 / 24 + yrs.old
+  str.mid <- str.mnth / 12 + 1 / 24 + yrs.old
+  end.mid <- str.mid + 1
+
+
+  # Assess whether the parameters at this update cross the y=0 axis
+  # This will allow us to assess whether we need to calculate roots 
+  # for the seasonal function or not
+  
+  time      <- seq(0, 1, 0.01)
+  seas.func <- SeasFunc(k0, theta.1, theta.2, time)
+  is.neg.1  <- min(seas.func)
+  is.neg.2  <- max(seas.func)
+	  
+  if (is.neg.1 < 0 & is.neg.2 > 0) {
+
+
+	# Calculate the roots of the seasonal function 
+	
+    root.1 <- SeasRootCalc(k0, theta.1, theta.2, yrs.old)[1]
+    root.2 <- SeasRootCalc(k0, theta.1, theta.2, yrs.old)[2]
+
+	    
+	# Given the roots calculate the integral over the previous years
+	# This is equal to the integral up to the first root + the in
+	
+	int.root.1   <- SeasIntegFunc(k0, theta.1, theta.2, str.mid, root.1)
+	int.root.2   <- SeasIntegFunc(k0, theta.1, theta.2, root.2,  end.mid)
+	int.yrs.prvs <- yrs.old * (int.root.1 + int.root.2)
+	
+	    
+    # Integral for those months less than root 1
+    
+    if (mm.val <= root.1) {
+    	
+      integral = int.yrs.prvs 
+                 + SeasIntegFunc(k0, theta.1, theta.2, str.mid, mm.val)
+    }
+	    	
+    # Integral for those months greater than root 1 but less than root 2
+
+    if (mm.val > root.1 & mm.val < root.2) {
+    	
+    	integral = int.yrs.prvs 
+                   + SeasIntegFunc(k0, theta.1, theta.2, str.mid, root.1)
+    }
+	    
+    # Integral for those months less than root 1
+    
+    if (mm.val >= root.2) {
+    	
+       int.root.1.1 <- SeasIntegFunc(k0, theta.1, theta.2, str.mid, root.1)
+       int.root.2.1 <- SeasIntegFunc(k0, theta.1, theta.2, root.2,  mm.val)
+	   integral     <- int.root.1.1 + int.root.2.1 + int.yrs.prvs
+	  }
+	    
+    } else {
+    
+    # If the integral doesn't have a negative component
+    
+    integral = SeasIntegFunc(k0, theta.1, theta.2, str.mid, mm.val)
+    }
+	
+	 
+  if (month > 11) {
+    
+    mu.yr.2 + (linf - mu.yr.2)*(1-exp(-integral))
+    
+  } else {
+ 	
+ 	# For the months in the first year 
+    mu.yr.1 + (linf - mu.yr.1)*(1-exp(-integral))
+  }
+	
+	
+}
